0c2cdfdb587b5c6e063bcc824d77debf4f376baa
dhnushreddy <dhanrajkutwade7@gmail.com>
2024-05-21 01:23:57 +0530

    changes in main.js

---
+++ main.js
@@ -0,0 +1,52 @@
+/*
+#git is a vcs(version control system)
+#git init comment used to initialize git repo
+#git status shows what are changes made in the new commit compared to previous commit
+#git add .  -- stage all changes(confirm changes )
+#git commit -m 'any message '   --used to add files on git
+# git restore --staged file to resore (used to go previous state)
+
+#git checkout gitid (used to return previous comit)
+
+#git checkout main ---used to return lates coment
+
+#git clone  --used to cline git repo in local machine
+
+
+/* 
+#Git:
+Version Control System is a tools that helps to track changes in code
+Git is a Version Control System. It is :
+
+popular
+free & Open Source
+fast & scalable
+help in collaboration 
+track history of code
+
+#Github:
+    Website that allows developers to store and manage their code using Git.
+    https://github.com
+
+
+types of file in git :
+untracked:new files that git doesn't yet track
+modified:changed file
+unmodified:unchanged
+staged:file is ready to be committed
+
+Add & Commit:
+add - adds new or changed files in your working directory to the Git staging area.
+git add <- file name ->
+commit - it is the record of change
+git commit -m "some message"
+
+Push Command
+push - upload local repo content to remote repo
+git push origin main
+*/
+
+
+
+
+
